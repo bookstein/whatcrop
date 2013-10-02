1eb1f1bf5e7aa5ef355b91412bae65d1e46cd753
EmilyBoo <ehbookstein@gmail.com>
2013-10-02 15:54:03 -0700

    Ready for fulllength testing on whatcrop server

--- V1_master/mainscript.js
+++ V1_master/mainscript.js
@@ -22,7 +22,7 @@ game = {
 	meanHistoricWeather : 0,
 
 	// Set number of turns per game
-    maxturn : 10,
+    maxturn : 50,
 	//Turn Counter
 	turn : 0,
 	// Total length of each turn (in milliseconds) from clicking #grow button to new turn
